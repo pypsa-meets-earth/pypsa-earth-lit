3982c3bee8764e100b5d5e010a81ab4982cf6230
Max Parzen <max.parzen@ed.ac.uk>
2023-06-25 16:34:07 +0100

    fix typo

--- app/index.py
+++ app/index.py
@@ -5,7 +5,7 @@ import plotly.express as px
 st.title("Welcome to the Energy Transition Explorer!")
 
 
-st.header("select a page from the sidebar to get started")
+st.header("Select a page from the sidebar to get started")
 
 st.subheader("This is a prototype of the Energy Transition Explorer. You can view the code on [GitHub](https://github.com/pypsa-meets-earth/pypsa-earth-lit)")
 
