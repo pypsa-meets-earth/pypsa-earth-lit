3daf780484f90cf9a28ce3fffe6ef2a4f21566cf
ekatef <e.v.kasilova@gmail.com>
2023-08-15 20:32:29 +0300

    Fix an order of colors for scenarios plots

--- app/pages/1_Scenario_comparison.py
+++ app/pages/1_Scenario_comparison.py
@@ -11,10 +11,19 @@ import app.pages.utils.scenario_comparision_prerun as helper
 def get_stat_unit(param):
     return tools.config["statistics_param_units"][param]
 
+def get_carrier_map():
+    return tools.config["carrier"]
+
+def get_colors_map():
+    return tools.config["tech_colors"]
+
 def main():
     st.title("Scenario comparison")
 
     network_map = tools.get_network_map()
+
+    carriers_map = get_carrier_map()
+    tech_map = dict(map(reversed, carriers_map.items()))
 
     ###### first dropdown plotting n.statistics #####
     params = list(network_map.values())[0].statistics().columns
@@ -25,12 +34,17 @@ def main():
     df = helper.get_df_for_parameter(
         network_map, option, helper.add_values_for_statistics, helper.get_stats_col_names
     )
+
+    df_techs = [tech_map[c] for c in df.columns]
+    tech_colors = get_colors_map()
+    plot_color = [tech_colors[c] for c in df_techs]
     _, plot_col,_ = st.columns([1, 80, 1])
     with plot_col:
         if option == "Capacity Factor":
             st.plotly_chart(px.scatter(df, y=df.columns,
                 # TODO Would be nice to adjust markers
                 # size=[20],
+                color_discrete_sequence=plot_color,
                 labels={
                     "value":get_stat_unit(option),
                     "index":"scenarios"
@@ -38,6 +52,7 @@ def main():
             )
         else:
             st.plotly_chart(px.bar(df, y=df.columns, 
+                color_discrete_sequence=plot_color,                    
                 labels={
                     "value":get_stat_unit(option),
                     "index":"scenarios"
@@ -56,15 +71,23 @@ def main():
             "co2_emissions",
             helper.add_values_for_co2,
             helper.get_co2_col_names,
-        )
+        ).drop("Load", axis=1)
 
-        _,plot_col,_=st.columns([1,60,1])
+        df_techs = [tech_map[c] for c in co2_df.columns]
+        tech_colors = get_colors_map()
+        plot_color = [tech_colors[c] for c in df_techs]
+
+        _,plot_col,_ = st.columns([1,60,1])
 
         with plot_col:
-            fig = px.bar(co2_df, y=co2_df.columns, title="CO2 emissions",labels={
-            "value":tools.config["second_param_units"][option],
-            "index":"scenarios"
-        })
+            fig = px.bar(co2_df, y=co2_df.columns, 
+                title="CO2 emissions",
+                color_discrete_sequence=plot_color,
+                labels={
+                    "value":tools.config["second_param_units"][option],
+                    "index":"scenarios"
+                }
+        )
             st.plotly_chart(fig, use_container_width=True)
 
     elif option == "Optimal Capacity":
@@ -72,13 +95,21 @@ def main():
             network_map, None, helper.add_values_for_generators, helper.get_gen_col_names
         )
 
+        df_techs = [tech_map[c] for c in gen_df.columns]
+        tech_colors = get_colors_map()
+        plot_color = [tech_colors[c] for c in df_techs]
+
         _,plot_col,_=st.columns([1,80,1])
 
         with plot_col:
-            fig = px.bar(gen_df, y=gen_df.columns, title="Optimal Capacity",labels={
-            "value":tools.config["second_param_units"][option],
-            "index":"scenarios"
-        })
+            fig = px.bar(gen_df, y=gen_df.columns, 
+                title="Optimal Capacity",
+                color_discrete_sequence=plot_color,
+                labels={
+                    "value":tools.config["second_param_units"][option],
+                    "index":"scenarios"
+                }
+        )
             st.plotly_chart(fig, use_container_width=True)
 
 
